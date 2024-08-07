f7096188e4fb8e3aaed4ecf1209c649868153bff
stacey csikos <stacey.csikos@datadoghq.com>
2024-08-07 14:22:24 -0400

    frontend working

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserInterface from "@/components/UserInterface"; 
+
+
+const Home : React.FC = () => {
+  return(
+    <div>
+      <UserInterface backendName="flask" />
+    </div>
+  )
+}
+
+export default Home;
